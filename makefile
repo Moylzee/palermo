ba06be6a537ceecd47cd83832cc80a3e3c1b8fbd
Dave Szczesny <dave.szczesny@genesys.com>
2025-05-22 17:13:58 +0100

    Added firebase + webpack

---
+++ makefile
@@ -0,0 +1,9 @@
+install:
+	npm install -g firebase-tools
+	npm init -y
+	npm install --save firebase
+	npm install --save-dev webpack webpack-cli webpack-dev-server html-webpack-plugin
+
+deploy:
+	npx webpack
+	firebase deploy
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    mode: "development",
+    devServer: {
+        static: "./dist",
+        port: 3000,
+        open: true,
+    },
+    module: {
+        rules: [],
+    }
+};
